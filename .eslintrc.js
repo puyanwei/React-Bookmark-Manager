98e2e1d971d4cf8be7451973a5ff7e2cfda80287
Puyan Wei <puyanwei@gmail.com>
2018-06-20 09:30:08 +0100

    Form captures value in textbox and outputs it to console

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
         "es6": true
     },
     "extends": ["eslint:recommended", "plugin:react/recommended", "plugin:jest/recommended"
+    "parser": "babel-eslint",
     ],
     "parserOptions": {
         "ecmaFeatures": {
--- src/components/BookmarkForm.js
+++ src/components/BookmarkForm.js
@@ -3,14 +3,22 @@ import React from 'react';
 class BookmarkForm extends React.Component {
 	render() {
 		return (
-			<div className="bookmark-form">
-				<form>
-					<input placeholder="Add website address" />
+			<div className="bookmark-form-container">
+				<form className="bookmark-form" onSubmit={this.handleSubmit}>
+					<input placeholder="Add website address" ref="webAddress"
+						required
+					/>
 					<input type="submit" value="submit" />
 				</form>
 			</div>
 		);
 	}
-}
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.refs.webAddress.value);
+        this.refs.webAddress.value = ``;
+    }
+};
+
 
 export default BookmarkForm;
