0d1aaca6f890c91bfb3d723377fc8c8801ab305a
Puyan Wei <puyanwei@gmail.com>
2018-06-20 12:20:55 +0100

    Added form validation only accepting website urls, created error message and styled red

--- src/components/BookmarkForm/BookmarkForm.js
+++ src/components/BookmarkForm/BookmarkForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import style from './BookmarkForm.css';
 
 class BookmarkForm extends React.Component {
 	render() {
@@ -12,14 +13,25 @@ class BookmarkForm extends React.Component {
 					/>
 					<input type="submit" value="submit" />
 				</form>
+				<div id="error-message" />
 			</div>
 		);
 	}
 	handleSubmit = (event) => {
 		event.preventDefault();
-		this.props.onAdd(this.refs.webAddress.value);
-		this.refs.webAddress.value = '';
+		if (this.emailValidation()) {
+			this.props.onAdd(this.refs.webAddress.value);
+			this.refs.webAddress.value = '';
+			document.getElementById('error-message').innerText = '';
+		} else {
+			document.getElementById('error-message').innerText =
+				'Not a valid website url';
+		}
 	};
+	emailValidation() {
+		let websiteRegex = /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$/;
+		return websiteRegex.test(this.refs.webAddress.value);
+	}
 }
 
 export default BookmarkForm;
